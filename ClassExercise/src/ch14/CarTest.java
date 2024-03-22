e58c95011ca2330f5affbd694cc8dfee38d153f2
JerryWang0122 <school832@gmail.com>
2024-03-22 14:28:22 +0800

    Optimize: 加入老師上課覺得可以改進的地方

--- ClassExercise/src/ch14/CarTest.java
+++ ClassExercise/src/ch14/CarTest.java
@@ -1,48 +1,48 @@
 package ch14;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class CarTest {
-    static Set<String> carBrand = new HashSet<>();
-    static ArrayList<String> carProduct = new ArrayList<>();
+    static Set<String> carBrands = new TreeSet<>();
+    static List<String> garage = new LinkedList<>();
 
     public static void main(String[] args) {
+        System.out.println("採購車輛...");
         Scanner scanner = new Scanner(System.in);
         String input;
-        while (carBrand.size() < 3) {
+        while (garage.size() < 8) {
             System.out.print("請輸入汽車品牌：");
-            input = scanner.next();
-            if (carBrand.add(input)) {
+            input = scanner.nextLine();
+            if (carBrands.add(input)) {
                 System.out.println("新增品牌：" + input);
             } else {
                 System.out.println("現有品牌：" + input);
             }
-            carProduct.add(input);
-
+            garage.add(input);
         }
-        System.out.println("銷售品牌：" + carBrand);
-        System.out.println("現有車輛：" + carProduct);
+        printData();
+        System.out.println("銷售車輛......");
         while (true) {
-            if (carProduct.isEmpty()) break;
-            System.out.print("輸入欲購買品牌：");
-            input = scanner.next();
-            if (input.equals("Quit")) break;
-            int index = carProduct.indexOf(input);
+            if (garage.isEmpty()) break;
+            System.out.print("輸入欲購買品牌(輸入Quit結束)：");
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("Quit")) break;
+            int index = garage.indexOf(input);
             if (index == -1) {
                 System.out.println("未銷售" + input);
             } else {
                 System.out.println("請至" + index + "號車庫賞車");
-                carProduct.remove(index);
-                if (!carProduct.contains(input)) carBrand.remove(input);
+                garage.remove(index);
+                if (!garage.contains(input)) carBrands.remove(input);
                 System.out.println(input + "已銷售");
             }
         }
-        System.out.println("銷售品牌：" + carBrand);
-        System.out.println("現有車輛：" + carProduct);
-
+        printData();
         scanner.close();
+    }
+
+    private static void printData() {
+        System.out.println("銷售品牌：" + carBrands);
+        System.out.println("現有車輛：" + garage);
     }
 }
